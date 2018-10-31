68f90421ffa1fa2538760e92e19e475d6b63c8d6
Time <time.sbs@gmail.com>
2018-10-31 14:44:34 +0700

    Add Create,Remove,getID for Dogs

--- src/controllers/Dog/Dogs.js
+++ src/controllers/Dog/Dogs.js
@@ -1,5 +1,6 @@
 const Dogs = require('../../models/Dog')
 
+//Get all dogs
 const getDogs = (req,res) => {
     Dogs.find({
 
@@ -13,7 +14,35 @@ const getDogs = (req,res) => {
         }
     });
 };
-
+//Find all Dogs that for Sale
+const getDogsFS = (req,res) => {
+    Dogs.find({
+        type: '1' //type 1 = forSale
+    }).exec((err, dog) => {
+        if(err) {
+            res.status(404).send();
+            return;
+        }
+        else {
+            res.status(200).json(dog);
+        }
+    });
+};
+//Find all dogs that for Adopt
+const getDogsFA = (req,res) => {
+    Dogs.find({
+        type: '2' //type 2 = forAdopt
+    }).exec((err, dog) => {
+        if(err) {
+            res.status(404).send();
+            return;
+        }
+        else {
+            res.status(200).json(dog);
+        }
+    });
+};
+//Find dog by id
 const getDogId = (req,res) => {
     Dogs.findOne({
         id: req.params.id
@@ -27,7 +56,7 @@ const getDogId = (req,res) => {
         }
     });
 };
-
+//Delete one dog
 const deleteDog = (req,res) => {
     Dogs.deleteOne({
         id: req.params.id
@@ -41,7 +70,51 @@ const deleteDog = (req,res) => {
     })
 
 }
+//Create Dog
+const createDog = (req,res) => {
+    const dogData = req.body;
+    Dogs.create({
+        name: dogData.name,
+        birthDate: dogData.birthDate,
+        breed: dogData.breed,
+        dataBreed: dogData.dataBreed,
+        momBreed: dogData.momBreed,
+        shopId: dogData.shopId,
+        selledDate: dogData.selledDate,
+        description: dogData.description,
+        pictures: dogData.pictures,
+        size: dogData.size,
+        weight: dogData.weight,
+        primaryColor: dogData.primaryColor,
+        secondaryColor: dogData.secondaryColor,
+        type: dogData.type,
+        certificateId: dogData.certificateId,
+        sellPrice: dogData.sellPrice,
+        rentPrice: dogData.rentPrice,
+        rentStatus: dogData.rentStatus
+    },function(err){
+        if(err){
+            res.json({
+                msg: 'Failed to create new dog'
+            });
+        }
+        else{
+            res.status('200').json({
+                msg: "Add " + dogData.name + " sucessful",
+                id: dogData.id
+            });
+        }
+    })
+}
+
+
+
 module.exports = {
-    getDogs: getDogs
+    getDogs:   getDogs,
+    createDog: createDog,
+    getDogsFA: getDogsFA,
+    getDogsFS: getDogsFS,
+    deleteDog: deleteDog,
+    getDogId:  getDogId
 }
 
